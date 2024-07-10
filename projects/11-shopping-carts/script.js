aa0fc561b6b71370740c52139c41a5c289df6308
NatalieShif <natalia.shif@gmail.com>
2024-07-10 15:58:37 +0300

    Shopping Cart more details and one more example picture

--- projects/11-shopping-carts/script.js
+++ projects/11-shopping-carts/script.js
@@ -2,3 +2,22 @@ const avatarStockEl = document.querySelector(`.customer-avatars .stock`);
 const cartStockEl = document.querySelector(`.carts .stock`);
 const productShelvesEl = document.querySelector(`.products .stock`);
 
+
+class Stock {
+
+   constructor() {
+
+   }
+
+
+}
+
+const clientAvatarStock = new Stock(avatarStockEl,avatarIconNames);
+
+const newIcon = document.createElement('i');
+
+const newClientAvatar = new Avatar(newIcon,iconClasses);
+
+const cartAvatarStock = new Stock(cartStockEl, cartIconNames);
+
+class ProductStock extends Stock
