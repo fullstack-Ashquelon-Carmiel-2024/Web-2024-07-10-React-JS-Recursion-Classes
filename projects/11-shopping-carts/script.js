cccef0ccdba3aad13a4a1788a7a2180890635024
NatalieShif <natalia.shif@gmail.com>
2024-07-10 15:17:02 +0300

    Shopping Cart proj details

---
+++ projects/11-shopping-carts/script.js
@@ -0,0 +1,4 @@
+const avatarStockEl = document.querySelector(`.customer-avatars .stock`);
+const cartStockEl = document.querySelector(`.carts .stock`);
+const productShelvesEl = document.querySelector(`.products .stock`);
+
